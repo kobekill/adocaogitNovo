b78b0f2361dd288c8c9f87638ed2a11c46cd9e42
Luis Felipe Loch <luisfelipe_lfl@hotmail.com>
2019-11-13 22:52:37 -0200

    start

---
+++ src/test/java/br/com/animais/adocao/dao/teste/CadastroOngBean.java
@@ -0,0 +1,15 @@
+package br.com.animais.adocao.dao.teste;
+
+
+class CadastroOngBean {
+
+	private br.com.animais.adocao.bean.CadastroOngBean ongBean; 
+	
+	
+	void testOngBean() {
+		ongBean = new br.com.animais.adocao.bean.CadastroOngBean();
+		ongBean.inicializar();
+		
+	}
+
+}
